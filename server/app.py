bc14170065ba048e615616d14f96e0e46b6fe292
Hiroki Kato <hirokikato1@gmail.com>
2023-09-24 15:45:42 -0700

    completes lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,18 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    all_articles = [a.to_dict() for a in Article.query.all()]
+    return make_response(jsonify(all_articles), 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1 
+    
+    if session['page_views'] <= 3:
+       return Article.query.filter(Article.id == id).first().to_dict(), 200
+    else:
+        return make_response({'message': 'Maximum pageview limit reached'},401)
 
 if __name__ == '__main__':
     app.run(port=5555)
